928c051add045cf8f2e3ae2de2038c3c402ab26f
Ben <bengoehring1@gmail.com>
2019-11-01 08:28:25 -0400

    first commit

---
+++ party_control/party_control.R
@@ -0,0 +1,86 @@
+library(tidyverse)
+
+BeRn::set_my_theme()
+
+# Read in main data from 1980 to 2011 ####
+raw_data <- readxl::read_xlsx("correlatesofstatepolicyprojectv2_1.xlsx",
+                             sheet = "Government_edit")
+
+# data only goes through 2011 - note dropping DC and NE
+final_data <- raw_data %>% 
+  filter(year >= 1980 & year < 2012) %>% 
+  filter(st != "DC" & st != "NE") 
+
+# national counts of rep and dem control
+final_data <- final_data %>% 
+  group_by(year) %>% 
+  mutate(nat_dem_unified = sum(dem_unified)) %>% 
+  mutate(nat_rep_unified = sum(rep_unified)) %>% 
+  ungroup()
+
+final_data_long <- final_data %>% 
+  select(year, state, nat_dem_unified, nat_rep_unified) %>% 
+  pivot_longer(cols = nat_dem_unified:nat_rep_unified,
+               names_to = "party_control",
+               values_to = "number_govs") %>% 
+  mutate(party_control = ifelse(party_control == "nat_dem_unified", "Democrat",
+                                ifelse(party_control == "nat_rep_unified", "Republican",
+                                       "ERROR")))
+
+# just keep national level data
+final_data_long <- final_data_long %>% 
+  select(year, party_control, number_govs) %>% 
+  distinct()
+
+# Read in NCSL data from 2012 to 2017 ####
+sheets <- as.character(2012:2017)
+ncsl_data <- map(sheets, ~ readxl::read_xlsx("ncsl/control_2012_2017.xlsx",
+                                             sheet = .,
+                                             skip = 1))
+
+ncsl_data_df <- reduce(ncsl_data, rbind)
+
+ncsl_data_df <- ncsl_data_df %>% 
+  filter(STATE != "Nebraska") %>% 
+  rename(year = Year, state_control = `State Control`) %>% 
+  mutate(rep_unified = ifelse(state_control == "Rep", 1, 0)) %>% 
+  mutate(dem_unified = ifelse(state_control == "Dem", 1, 0))
+
+# create annual counts
+ncsl_data_df <- ncsl_data_df %>% 
+  group_by(year) %>% 
+  mutate(nat_dem_unified = sum(dem_unified)) %>% 
+  mutate(nat_rep_unified = sum(rep_unified)) %>% 
+  ungroup() %>% 
+  select(year, nat_dem_unified, nat_rep_unified) %>% 
+  distinct()
+
+# Pivot to long 
+ncsl_long <- ncsl_data_df %>% 
+  pivot_longer(nat_dem_unified:nat_rep_unified,
+               names_to = "party_control",
+               values_to = "number_govs") %>% 
+  mutate(party_control = ifelse(party_control == "nat_rep_unified", "Republican",
+                                ifelse(party_control == "nat_dem_unified", "Democrat",
+                                       "ERROR")))
+
+
+# Join ncsl to original data from 1980 to 2011
+final_data_long <- rbind(final_data_long, ncsl_long)
+
+# plot
+final_data_long %>% 
+  ggplot() +
+  geom_line(aes(year, number_govs, color = party_control)) +
+  scale_color_manual(values = c("Democrat" = "blue", "Republican" = "red")) +
+  theme(legend.title = element_blank()) +
+  xlab(NULL) +
+  ylab("State governments under unified control") +
+  labs(title = "State governments under unified partisan control", 
+       subtitle = "1980-2017",
+       caption = "Note: Excludes Nebraska. Sources: Correlates of State Policy Project and National Conference of State Legislatures")
+  
+
+
+
+    
